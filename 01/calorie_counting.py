4b33ea66bd8bd82b6f08baa5799440746d810394
Tim Wende <twende@amt.rwth-aachen.de>
2022-12-01 10:05:46 +0100

    01 done

---
+++ 01/calorie_counting.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+with open('data.txt') as f:
+    file = f.readlines()
+file = [line.strip() for line in file]
+
+index = [-1]
+index.extend([i for i, x in enumerate(file) if x == ""])
+index.append(len(file))
+
+data = []
+for i in range(len(index)-1):
+    data.append(np.sum([int(val) for val in file[index[i]+1:index[i+1]]]))
+
+def part_one():
+    print("--- part one ---")
+    print(np.max(data))
+
+def part_two():
+    print("--- part two ---")
+    data.sort()
+    print(np.sum([data[i] for i in [-1, -2, -3]]))
+
+part_one()
+part_two()
