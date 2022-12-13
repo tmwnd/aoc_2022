9b7101f6fafe1fa9df45b78c7100ffb19c243b6b
Tim Wende <timwende040501@outlook.de>
2022-12-13 20:45:32 +0100

    13 done

--- 13/distress_signal.py
+++ 13/distress_signal.py
@@ -1,6 +1,7 @@
 with open("data.txt") as f:
     file = [line.strip() for line in f.readlines()]
 
+
 def make_packet(s):
     if "[" not in s:
         return int(s)
@@ -21,15 +22,19 @@ def make_packet(s):
 
     return packet
 
+
 data = []
 for signal in file:
     if signal != "":
         data.append(make_packet(signal))
 
+
 def compare_signals(left, right):
     def type_value(obj):
-        if type(obj) == int: return 0
-        if type(obj) == list: return 1
+        if type(obj) == int:
+            return 0
+        if type(obj) == list:
+            return 1
 
     type_values = [type_value(left), type_value(right)]
     match type_values:
@@ -41,22 +46,31 @@ def compare_signals(left, right):
             return compare_signals([left], right)
         case [1, 1]:
             for i in range(max(len(left), len(right))):
-                if i >= len(left): return -1
-                if i >= len(right): return 1
+                if i >= len(left):
+                    return -1
+                if i >= len(right):
+                    return 1
 
                 y = compare_signals(left[i], right[i])
-                if y != 0: return y
+                if y != 0:
+                    return y
             return 0
+
 
 def part_one():
     print("--- part one ---")
-    print(sum([(i+1) * int((compare_signals(data[2*i], data[2*i+1]) <= 0)) for i in range(len(data)//2)]))
+    print(sum([(i+1) * int((compare_signals(data[2*i], data[2*i+1]) <= 0))
+          for i in range(len(data)//2)]))
+
 
 def part_two():
     print("--- part two ---")
+
     data.append([[2]])
     data.append([[6]])
-    print(sorted(data, cpr=compare_signals))
+    print(sum([int(compare_signals(signal, [[2]]) <= 0) for signal in data])
+          * sum([int(compare_signals(signal, [[6]]) <= 0) for signal in data]))
+
 
 part_one()
 part_two()
